1e7682a51d76b6fcb7cadbfe030c0b2319186110
unknown <unknown@example.com>
2018-10-24 19:47:51 +0800

    Initial Commit

---
+++ pages/dianhua/dianhua.js
@@ -0,0 +1,86 @@
+// pages/dianhua/dianhua.js
+Page({
+
+  phoneCall: function (e) {
+    console.log(e);
+    wx.makePhoneCall({
+      phoneNumber: e.currentTarget.id, //此号码仅用于测试 。
+      success: function () {
+        console.log("拨打电话成功！")
+      },
+      fail: function () {
+        console.log("拨打电话失败！")
+      }
+    })
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    phoneNum:{}
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    var that = this;
+    wx.request({
+      url: 'https://224r34952t.51mypc.cn/sdFindPhoneController',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      method: 'GET',
+      success: function (res) {
+        console.log(res);
+        var list = res.data;
+        if (list == null) {
+          var toastText = '获取数据失败' + res.data.errMsg;
+          wx.showToast({
+            title: toastText,
+            icon: '',
+            duration: 2000 //弹出时间
+          })
+        } else {
+          that.setData({
+            sdPhone:list,
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
